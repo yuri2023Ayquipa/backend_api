f4a26d77769bc428a902125c9a7c9d2e548cb69c
yuri2023Ayquipa <ayquipa26_04@outlook.com>
2024-08-12 16:08:57 -0500

    url1

--- core/urls.py
+++ core/urls.py
@@ -25,7 +25,7 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
-    authentication_classes=[TokenAuthentication],
+    #authentication_classes=[TokenAuthentication],
     
     )
 
